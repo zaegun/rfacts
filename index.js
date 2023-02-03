791d59ba3d62d23c1ca06d322c4cda290270712e
sozae <gdbarker@gmail.com>
2023-02-03 15:17:38 -0800

    functional base page

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+const title = "Fun facts about React"
+
 function Header() {
     return(
-        <h1>Hello, World</h1>
+        <header>
+            <nav>
+                <img src='react_logo.png' id="logo" width="40" />
+                <h1>{title}</h1>
+            </nav>
+        </header>
     )
 }
 
 function MainContent() {
     return(
-        <p>This is the content area</p>
+        <div>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originall created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+function Footer() {
+    return(
+        <footer>
+            <small>© 20xx Barker development. All rights reserved.</small>
+        </footer>
     )
 }
 
 const root = document.getElementById("root");
-ReactDOM.render(
+ReactDOM.createRoot(root).render(
     <div>
         <Header />
         <MainContent />
-    </div>, 
-    root);
+        <Footer />
+    </div>
+    );
